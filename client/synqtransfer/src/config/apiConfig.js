77475d99363d30c3e6ad986c389843425ddd2ed6
ashishvrsiis <ashish@vrsiis.com>
2025-06-26 21:55:22 +0530

    updated base url

--- client/synqtransfer/src/config/apiConfig.js
+++ client/synqtransfer/src/config/apiConfig.js
@@ -1,8 +1,8 @@
 // src/config/apiConfig.js
 
-// const BASE_URL = 'https://synqtransfer.onrender.com/api';
+const BASE_URL = 'https://synqtransfer.onrender.com/api';
 // export const BASE_URL = 'https://518dhcgx-5000.inc1.devtunnels.ms/api';
-export const BASE_URL = 'http://65.2.142.111/api';
+// export const BASE_URL = 'http://65.2.142.111/api';
 
 export const SOCKET_URL = BASE_URL.replace('/api', '');
 
